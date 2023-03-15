91c6854f2484e70c21955c0e17f357e993ac77bd
unknown <aldianreza46@gmail.com>
2023-03-15 12:30:50 +0700

    upload tugas praktikum

---
+++ modulaldian.py
@@ -0,0 +1,10 @@
+def greeting(name):
+    print("Hello, " + name) 
+
+person1 = {
+    "name": "Aldian Reza",
+    "NIM": 312010245,
+    "class": "TI20E1",
+    "age": 22,
+    "country": "Indonesia"
+}
